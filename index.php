b8f1521ea3201c20031d1442401fb03a966dbe17
ZhuK <127308032+ZhuK112@users.noreply.github.com>
2023-06-29 22:09:34 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Обчислення</title>
+</head>
+<body>
+    <form method="POST" action="process.php">
+        <label for="c">Число C:</label>
+        <input type="number" name="c" id="c">
+        <br>
+        <label for="d">Число D:</label>
+        <input type="number" name="d" id="d">
+        <br>
+        <input type="submit" value="Відправити">
+    </form>
+</body>
+</html>
